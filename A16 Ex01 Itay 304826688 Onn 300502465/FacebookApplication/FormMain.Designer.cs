59517cb3cab2c668688877fc7c33e8a33a214cdc
ItayAb <itayabra1@gmail.com>
2015-11-11 21:54:14 +0200

    Added Like Analyzer Class

--- A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/FormMain.Designer.cs
+++ A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/FormMain.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.picture_CoverPhoto = new System.Windows.Forms.PictureBox();
             this.picture_ProfilePhoto = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.ListView_Posts = new System.Windows.Forms.ListView();
+            this.button_GroupView = new System.Windows.Forms.Button();
+            this.button_LikeAnaylzer = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.buttonLogin = new System.Windows.Forms.Button();
+            this.button_Login = new System.Windows.Forms.Button();
+            this.listBox_Posts = new System.Windows.Forms.ListBox();
+            this.label_FullName = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picture_CoverPhoto)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picture_ProfilePhoto)).BeginInit();
             this.SuspendLayout();
@@ -54,38 +55,31 @@
             // 
             this.picture_ProfilePhoto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.picture_ProfilePhoto.Image = global::FacebookApplication.Properties.Resources.ProfileDefault;
-            this.picture_ProfilePhoto.Location = new System.Drawing.Point(260, 52);
+            this.picture_ProfilePhoto.Location = new System.Drawing.Point(12, 12);
             this.picture_ProfilePhoto.Name = "picture_ProfilePhoto";
             this.picture_ProfilePhoto.Size = new System.Drawing.Size(125, 125);
             this.picture_ProfilePhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.picture_ProfilePhoto.TabIndex = 1;
             this.picture_ProfilePhoto.TabStop = false;
             // 
-            // button1
+            // button_GroupView
             // 
-            this.button1.Location = new System.Drawing.Point(170, 187);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 26);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Group View";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_GroupView.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button_GroupView.Location = new System.Drawing.Point(282, 12);
+            this.button_GroupView.Name = "button_GroupView";
+            this.button_GroupView.Size = new System.Drawing.Size(94, 26);
+            this.button_GroupView.TabIndex = 2;
+            this.button_GroupView.Text = "Group View";
+            this.button_GroupView.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // button_LikeAnaylzer
             // 
-            this.button2.Location = new System.Drawing.Point(380, 190);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Like Analyzer";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // ListView_Posts
-            // 
-            this.ListView_Posts.Location = new System.Drawing.Point(158, 292);
-            this.ListView_Posts.Name = "ListView_Posts";
-            this.ListView_Posts.Size = new System.Drawing.Size(327, 363);
-            this.ListView_Posts.TabIndex = 4;
-            this.ListView_Posts.UseCompatibleStateImageBehavior = false;
+            this.button_LikeAnaylzer.Location = new System.Drawing.Point(411, 12);
+            this.button_LikeAnaylzer.Name = "button_LikeAnaylzer";
+            this.button_LikeAnaylzer.Size = new System.Drawing.Size(105, 26);
+            this.button_LikeAnaylzer.TabIndex = 3;
+            this.button_LikeAnaylzer.Text = "Like Analyzer";
+            this.button_LikeAnaylzer.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -112,28 +106,47 @@
             this.button4.Text = "Button 4";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // buttonLogin
+            // button_Login
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(279, 23);
-            this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(91, 23);
-            this.buttonLogin.TabIndex = 8;
-            this.buttonLogin.Text = "Login";
-            this.buttonLogin.UseVisualStyleBackColor = true;
-            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            this.button_Login.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button_Login.Location = new System.Drawing.Point(158, 12);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(91, 26);
+            this.button_Login.TabIndex = 8;
+            this.button_Login.Text = "Login";
+            this.button_Login.UseVisualStyleBackColor = false;
+            this.button_Login.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // listBox_Posts
+            // 
+            this.listBox_Posts.FormattingEnabled = true;
+            this.listBox_Posts.Location = new System.Drawing.Point(158, 292);
+            this.listBox_Posts.Name = "listBox_Posts";
+            this.listBox_Posts.Size = new System.Drawing.Size(327, 264);
+            this.listBox_Posts.TabIndex = 9;
+            // 
+            // label_FullName
+            // 
+            this.label_FullName.AutoSize = true;
+            this.label_FullName.Location = new System.Drawing.Point(70, 198);
+            this.label_FullName.Name = "label_FullName";
+            this.label_FullName.Size = new System.Drawing.Size(0, 15);
+            this.label_FullName.TabIndex = 10;
+            this.label_FullName.AutoSize = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(651, 654);
-            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.label_FullName);
+            this.Controls.Add(this.listBox_Posts);
+            this.Controls.Add(this.button_Login);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.ListView_Posts);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_LikeAnaylzer);
+            this.Controls.Add(this.button_GroupView);
             this.Controls.Add(this.picture_ProfilePhoto);
             this.Controls.Add(this.picture_CoverPhoto);
             this.Name = "FormMain";
@@ -149,13 +162,14 @@
 
         private System.Windows.Forms.PictureBox picture_CoverPhoto;
         private System.Windows.Forms.PictureBox picture_ProfilePhoto;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListView ListView_Posts;
+        private System.Windows.Forms.Button button_GroupView;
+        private System.Windows.Forms.Button button_LikeAnaylzer;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.ListBox listBox_Posts;
+        private System.Windows.Forms.Label label_FullName;
     }
 }
 
