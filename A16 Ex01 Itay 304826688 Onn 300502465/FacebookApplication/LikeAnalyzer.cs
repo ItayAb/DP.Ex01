721a143ef602458667b56a84400869eaf403d73f
ItayAb <itayabra1@gmail.com>
2015-11-11 22:29:23 +0200

    Added LikeAnalyzer functionality

---
+++ A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/LikeAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+namespace FacebookApplication
+{
+    class LikeAnalyzer
+    {
+        Dictionary<User, int> m_LikeDataAnalysis;
+        User m_LoggedUser;
+
+        public LikeAnalyzer(User i_LoggedUser)
+        {
+            m_LikeDataAnalysis = new Dictionary<User, int>();
+            m_LoggedUser = i_LoggedUser;
+        }
+
+        private void calculateLikeToList()
+        {    
+            // iterating all the posts 
+            foreach (Post postCurrentlyCalculating in m_LoggedUser.Posts)
+            {
+                // iterating all user who liked the post
+                foreach (User userWhoLikedThePost in postCurrentlyCalculating.LikedBy)
+                {
+                    // if the user already appeared in previous calculations
+                    if (m_LikeDataAnalysis.ContainsKey(userWhoLikedThePost))
+                    {
+                        int likeCountForCurrentCalculatedUser = m_LikeDataAnalysis[userWhoLikedThePost];
+                        likeCountForCurrentCalculatedUser++;
+                        m_LikeDataAnalysis[userWhoLikedThePost] = likeCountForCurrentCalculatedUser;
+                    }
+                    else
+                    {
+                        m_LikeDataAnalysis.Add(userWhoLikedThePost, 1);
+                    }
+                }
+            }
+        }
+
+        public List<User> GetDescendingTopLikeUserList()
+        {
+            List<User> topLikeUsers = new List<User>();
+
+            foreach (KeyValuePair<User, int> currentPairInCalculatedData in m_LikeDataAnalysis.OrderByDescending(Key => Key.Value ))
+            {
+                topLikeUsers.Add(currentPairInCalculatedData.Key);
+            }
+
+            return topLikeUsers;
+        }
+              
+    }
+}
--- A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/LikeAnalyzerForm.Designer.cs
+++ A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/LikeAnalyzerForm.Designer.cs
@@ -28,19 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBox_DescendingLikeFriends = new System.Windows.Forms.ListBox();
+            this.PictureBox_SelectedFriend = new System.Windows.Forms.PictureBox();
+            this.listBox_RecentPost = new System.Windows.Forms.ListBox();
+            this.button_LikeBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_SelectedFriend)).BeginInit();
             this.SuspendLayout();
+            // 
+            // listBox_DescendingLikeFriends
+            // 
+            this.listBox_DescendingLikeFriends.FormattingEnabled = true;
+            this.listBox_DescendingLikeFriends.Location = new System.Drawing.Point(12, 200);
+            this.listBox_DescendingLikeFriends.Name = "listBox_DescendingLikeFriends";
+            this.listBox_DescendingLikeFriends.Size = new System.Drawing.Size(120, 212);
+            this.listBox_DescendingLikeFriends.TabIndex = 0;
+            // 
+            // PictureBox_SelectedFriend
+            // 
+            this.PictureBox_SelectedFriend.Location = new System.Drawing.Point(175, 200);
+            this.PictureBox_SelectedFriend.Name = "PictureBox_SelectedFriend";
+            this.PictureBox_SelectedFriend.Size = new System.Drawing.Size(100, 86);
+            this.PictureBox_SelectedFriend.TabIndex = 1;
+            this.PictureBox_SelectedFriend.TabStop = false;
+            // 
+            // listBox_RecentPost
+            // 
+            this.listBox_RecentPost.FormattingEnabled = true;
+            this.listBox_RecentPost.Location = new System.Drawing.Point(175, 317);
+            this.listBox_RecentPost.Name = "listBox_RecentPost";
+            this.listBox_RecentPost.Size = new System.Drawing.Size(200, 95);
+            this.listBox_RecentPost.TabIndex = 2;
+            // 
+            // button_LikeBack
+            // 
+            this.button_LikeBack.Location = new System.Drawing.Point(300, 263);
+            this.button_LikeBack.Name = "button_LikeBack";
+            this.button_LikeBack.Size = new System.Drawing.Size(75, 23);
+            this.button_LikeBack.TabIndex = 3;
+            this.button_LikeBack.Text = "Like Back";
+            this.button_LikeBack.UseVisualStyleBackColor = true;
             // 
             // LikeAnalyzerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(490, 500);
+            this.Controls.Add(this.button_LikeBack);
+            this.Controls.Add(this.listBox_RecentPost);
+            this.Controls.Add(this.PictureBox_SelectedFriend);
+            this.Controls.Add(this.listBox_DescendingLikeFriends);
             this.Name = "LikeAnalyzerForm";
-            this.Text = "LikeAnalyzerForm";
+            this.Text = "Like Analyzer ";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox_SelectedFriend)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox_DescendingLikeFriends;
+        private System.Windows.Forms.PictureBox PictureBox_SelectedFriend;
+        private System.Windows.Forms.ListBox listBox_RecentPost;
+        private System.Windows.Forms.Button button_LikeBack;
     }
 }
