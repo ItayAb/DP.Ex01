42922c881a5bf049ed82af8c9fd044261203a7cb
onncho <onncho@outlook.com>
2015-11-19 00:51:53 +0200

    design forms1

--- A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/FormMain.Designer.cs
+++ A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/FormMain.Designer.cs
@@ -206,6 +206,7 @@
             this.Controls.Add(this.pictureCoverPhoto);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Facebook Application";
             ((System.ComponentModel.ISupportInitialize)(this.pictureCoverPhoto)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureProfilePhoto)).EndInit();
--- A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/MusicForm.Designer.cs
+++ A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/MusicForm.Designer.cs
@@ -225,6 +225,7 @@
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "MusicForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MusicForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYouTube)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFacebook)).EndInit();
