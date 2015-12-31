80482fcc064e96579a60b036ec98c8e409e9bf19
onncho <onncho@outlook.com>
2015-12-31 03:21:25 +0200

    desig

---
+++ A16 Ex01 Itay 304826688 Onn 300502465/FacebookApplication/FacbookMusicPages.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+
+namespace FacebookApplication
+{
+    class FacbookMusicPages : IFetchMusicData<Page>
+    {
+        private User m_LoggedUserAdoptee;
+        private List<Page> m_MusicPagesList;
+
+        public FacbookMusicPages(User i_LoggedUser)
+        {
+            m_LoggedUserAdoptee = i_LoggedUser;
+        }
+        
+       
+        public List<Page> fetch()
+        {
+            m_MusicPagesList = new List<Page>();
+
+            foreach (Page page in m_LoggedUserAdoptee.LikedPages)
+            {
+                if (page.Category == "Musician/Band")
+                {
+                    m_MusicPagesList.Add(page);
+                }
+            }
+
+            return m_MusicPagesList;
+        }
+    }
+}
